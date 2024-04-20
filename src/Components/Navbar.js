b134d5c79d8bee4827973c0b2923d06c0ed67222
tushart29 <tushar29t@gmail.com>
2024-04-19 19:03:28 -0700

    new files added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-links">
+                <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/things" activeClassName="active">Things to Read</NavLink></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/Components/Things.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../CSS/Things.css'
+
+const Things = () => {
+    return (
+        <div className='things-list'>
+            <ul>
+                <li><a href="https://www.google.com">Google</a></li>
+                <li><a href="https://www.facebook.com">Facebook</a></li>
+                <li><a href="https://www.youtube.com">YouTube</a></li>
+                <li><a href="https://www.twitter.com">Twitter</a></li>
+                <li><a href="https://www.instagram.com">Instagram</a></li>
+                <li><a href="https://www.wikipedia.org">Wikipedia</a></li>
+                <li><a href="https://www.amazon.com">Amazon</a></li>
+                <li><a href="https://www.reddit.com">Reddit</a></li>
+                <li><a href="https://www.linkedin.com">LinkedIn</a></li>
+                <li><a href="https://www.netflix.com">Netflix</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Things;
